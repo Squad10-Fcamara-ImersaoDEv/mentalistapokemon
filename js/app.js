cfa1ce6eae61b8539c134b3b73abc91527f701e6
stenioas <stenioas@gmail.com>
2021-04-29 23:30:58 -0300

    Refactor(app): function showPokemon added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 // ===== VARIABLES
 let baseUrl = 'https://pokeapi.co/api/v2/pokemon/'
+
 // Gera um número inteiro aleatório entre 1 e 898
-function getPokemonIndex() {
+function getRandomPokemon() {
     min = Math.ceil(1);
     max = Math.floor(898);
     return Math.floor(Math.random() * (max - min)) + min;
@@ -13,90 +14,26 @@ function getPokemonIndex() {
 // Seta o tempo para 30 seg
 
 // Carrega o card da sorte
-async function getPokemon(pokemonId) {
-    let id
+async function showPokemon(id) {
     let pokemonData = {};
-    let el = document.querySelector(".cards")
 
-    await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonId)
+    await fetch(baseUrl + id)
         .then(response => response.json())
         .then(data => {
             pokemonData.id = data.id
             pokemonData.name = data.name
             pokemonData.types = data.types.map(types => { return types.type.name; })
-            pokemonData.image = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + ("00" + pokemonId).slice(-3) + ".png"
-            id = ("00" + pokemonId).slice(-3)
-
-            switch (pokemonData.types[0]) {
-                case "grass":
-                    var classeTipoPrimarioPokemon = "card-grass";                    
-                    break;
-                case "fire":
-                    var classeTipoPrimarioPokemon = "card-fire";                        
-                    break;
-                case "water":
-                    var classeTipoPrimarioPokemon = "card-water";                  
-                    break;
-                case "poison":
-                    var classeTipoPrimarioPokemon = "card-poison";                  
-                    break;
-                case "psychic":
-                    var classeTipoPrimarioPokemon = "card-psychic"
-                    break;
-                case "ground":
-                    var classeTipoPrimarioPokemon = "card-ground"
-                    break;
-                case "electric":
-                    var classeTipoPrimarioPokemon = "card-electric"
-                    break; 
-                case "flying":
-                    var classeTipoPrimarioPokemon = "card-flying"
-                    break;
-                case "ice" :
-                    var classeTipoPrimarioPokemon = "card-ice"
-                    break;
-                case "bug" :
-                    var classeTipoPrimarioPokemon = "card-bug"
-                    break;    
-                case "dark" :
-                    var classeTipoPrimarioPokemon = "card-dark"
-                    break;    
-                case "dragon" :
-                    var classeTipoPrimarioPokemon = "card-dragon"
-                    break;  
-                case "fairy" :
-                    var classeTipoPrimarioPokemon = "card-fairy"
-                    break;
-                case "fighting" :
-                    var classeTipoPrimarioPokemon = "card-fighting"
-                    break;         
-                case "ghost" :
-                    var classeTipoPrimarioPokemon = "card-ghost"
-                    break;  
-                case "rock" :
-                    var classeTipoPrimarioPokemon = "card-rock"
-                    break;   
-                case "steel" :
-                    var classeTipoPrimarioPokemon = "card-steel"
-                    break;   
-                default:
-                    var classeTipoPrimarioPokemon = "card-normal"
-                    break;
-            }
-
-            el.innerHTML += `
-                            <div class="card-pokemon ${classeTipoPrimarioPokemon}" id =${pokemonData.name}>
-                                <h4><strong class="nome">${pokemonData.name}</strong></h4>
-                                <img class="imagempokemon" src="${pokemonData.image}" alt="imagem do ${pokemonData.name}">
-                                <div class="container">
-                                    <span id="code-pokemon">#${id}</span>
-                                    <p id="type-pokemon">${pokemonData.types}</p>
-                                </div>
-                            </div>
-                            `
+            pokemonData.image = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + ("00" + id).slice(-3) + ".png"
+            id = ("00" + id).slice(-3)
         })
         .catch(() => {
-            console.log("Error request pokémon. Id: " + pokemonId)
+            console.log("Error request pokémon. Id: " + id)
         })
+
+    console.log(pokemonData.id)
+    console.log(pokemonData.name)
+    console.log(pokemonData.types[0])
     return pokemonData;
 }
+
+showPokemon(getRandomPokemon())
