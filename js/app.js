df4bdf809fa833f05138e06cd0dfefacaca9d7cd
Artur <artuhens@gmail.com>
2021-05-01 00:14:54 -0300

    feature:dictionary com as geracoes pokemon e a logica de selecionar difficuldade

--- js/app.js
+++ js/app.js
@@ -37,3 +37,50 @@ async function showPokemon(id) {
 }
 
 showPokemon(getRandomPokemon())
+
+//Geracoes de pokemon
+var Geracoes = {
+    1: {
+        inicio: 1,
+        fim: 151,
+    },
+    2: {
+        inicio: 152,
+        fim: 251,
+    },
+    3: {
+        inicio: 252,
+        fim: 386,
+    },
+    4: {
+        inicio: 387,
+        fim: 493,
+    },
+    5: {
+        inicio: 494,
+        fim: 649,
+    },
+    6: {
+        inicio: 650,
+        fim: 721,
+    },
+    7: {
+        inicio: 722,
+        fim: 807,
+    },
+    8: {
+        inicio: 808,
+        fim: 898,
+    },
+};
+
+function definirDificuldade(dificuldadeSelecionada) {
+    if (dificuldadeSelecionada == dificuldadeFacil || selectedDifficulty == dificuldadeNormal) {
+        imageDirectory = 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/'
+    } 
+    // nao funciona para geração 6/7/8
+    else if (dificuldadeSelecionada == dificuldadeDificil) {
+        imageDirectory = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/500.png'
+    }
+}
+
