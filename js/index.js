4212a4512d6bbe9c16dc022c07793c2afd972c2c
Artur <artuhens@gmail.com>
2021-04-30 21:03:22 -0300

    feature:adição do back-to-top no index.html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+//botao voltar ao topo com sensação de scroll
+var btn = document.querySelector("#back-to-top");
+btn.addEventListener('click', () => window.scrollTo({
+    top ,
+    behavior: 'smooth',
+}));
+
 document.addEventListener('DOMContentLoaded', function() {
     var stream = document.querySelector('.gallery__stream');
     var items = document.querySelectorAll('.gallery__item');
