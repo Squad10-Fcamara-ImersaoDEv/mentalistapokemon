8b55e960710081a275ad7dfdb299a02726adbfbc
stenioas <stenioas@gmail.com>
2021-05-02 16:08:34 -0300

    feat(pokedex): search function
    search implementation started

--- js/suggestions.js
+++ js/suggestions.js
@@ -1,5 +1,6 @@
 let suggestions = []
 
+// getting pokemon list on pokeapi
 const getPokemonsList = async function() {
     await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=898')
         .then(resolve => resolve.json())
@@ -11,3 +12,62 @@ const getPokemonsList = async function() {
 }
 
 getPokemonsList()
+
+// getting all required elements
+const cardsBox = document.querySelector(".cards")
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let apiLink;
+
+// if user press any key and release
+inputBox.onkeyup = (e) => {
+    let userData = e.target.value; //user enetered data
+    if(userData){
+        icon.onclick = () => {
+            apiLink = "https://pokeapi.co/api/v2/pokemon/" + userData;
+            
+            linkTag.setAttribute("href", apiLink);
+            console.log(apiLink);
+            linkTag.click();
+        }
+        
+        emptyArray = suggestions.filter((data)=>{
+            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase()); 
+        });
+        emptyArray = emptyArray.map((data)=>{
+            // passing return data inside li tag
+            return data = '<li>'+ data +'</li>';
+        });
+        searchWrapper.classList.add("active"); //show autocomplete box
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            //adding onclick attribute in all li tag
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active"); //hide autocomplete box
+    }
+}
+
+async function getPokemon(name) {
+    let pokemonData = {}
+
+    await fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonId)
+        .then(response => response.json())
+        .then(data => {
+            pokemonData.id = data.id
+            pokemonData.name = data.name
+            pokemonData.types = data.types.map(types => { return types.type.name; })
+            pokemonData.image = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + ("00" + pokemonId).slice(-3) + ".png"
+            id = ("00" + pokemonId).slice(-3)
+  
+        })
+        .catch(() => {
+            console.log("Error request pokémon. Id: " + pokemonId)
+        })
+    return pokemonData;
+}
