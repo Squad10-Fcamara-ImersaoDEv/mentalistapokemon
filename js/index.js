70bf51ad03f4860cf1581a8c2818a899e99f3648
Douglas Assis <74422534+eudouglasassis@users.noreply.github.com>
2021-05-05 09:01:38 -0300

    feature: adição do menu hamburguer na home

--- js/index.js
+++ js/index.js
@@ -30,4 +30,32 @@ function controlesVideo() {
 };
 window.addEventListener('load', controlesVideo, false);
 
+// Ativação menu hamburguer
 
+var menu = document.querySelector(".menu")
+var ham = document.querySelector(".ham")
+var xIcon = document.querySelector(".xIcon")
+var menuIcon = document.querySelector(".menuIcon")
+
+ham.addEventListener("click", toggleMenu)
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    xIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+var menuLinks = document.querySelectorAll(".menuLink")
+var divAp = document.querySelectorAll("#responsiveAp")
+
+menuLinks.forEach(
+  function (menuLink) {
+    menuLink.addEventListener("click", toggleMenu)
+  }
+)
